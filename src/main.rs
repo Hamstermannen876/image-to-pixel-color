e722c586805f83636aa28ef758c3a7ffa435a5ed
Felix Sjödin <felix@homeoffice.nu>
2025-11-15 21:43:35 +0100

    print pixel colors to console, closes #1, #2

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::env;
+use image::{self, GenericImageView, Rgba};
+use std::{collections::HashMap, env};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -6,6 +7,21 @@ fn main() {
         panic!("invalid command line argument count, only input image file");
     }
 
-    let image_path = args[0].as_str();
+    let image_path = args[1].as_str();
 
+    let img = image::ImageReader::open(image_path).unwrap().decode().unwrap();
+
+    let mut colors: HashMap<Rgba<u8>, u32> = HashMap::new();
+
+    for (_, _, pixel) in img.pixels() {
+        if colors.contains_key(&pixel) {
+            *colors.get_mut(&pixel).unwrap() += 1;
+        } else {
+            colors.insert(pixel, 1);
+        }
+    }
+
+    for value in colors {
+        println!("{:?}", value);
+    }
 }
