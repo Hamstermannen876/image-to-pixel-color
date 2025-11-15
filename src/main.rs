1cd8e4a54138351b3e08ff1e715da577df014fa4
Felix Sjödin <felix@homeoffice.nu>
2025-11-15 20:58:12 +0100

    get image path from command line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::env;
+
 fn main() {
-    println!("Hello, World!")
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        panic!("invalid command line argument count, only input image file");
+    }
+
+    let image_path = args[0].as_str();
+
 }
