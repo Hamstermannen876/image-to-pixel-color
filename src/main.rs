d98bb5b6e57471277e2ac8861ff740f0e5ecfb4f
Felix Sjödin <felix@homeoffice.nu>
2026-04-08 22:06:08 +0200

    Change names of command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use csv;
-use image::{
-    self, DynamicImage, GenericImageView, Pixel, Rgb, Rgba, imageops::FilterType::Nearest,
-};
+use image::{self, DynamicImage, GenericImageView, Pixel, Rgba, imageops::FilterType::Nearest};
 use std::{collections::HashMap, env, path};
 
 fn rgba_to_hex(rgba: Rgba<u8>) -> String {
@@ -72,6 +70,7 @@ fn edges_of_pixel(x: u32, y: u32, img: &DynamicImage) -> u32 {
 
 fn get_pixel_info(img: &DynamicImage) -> HashMap<Rgba<u8>, (u32, u32, u32)> {
     let mut colors: HashMap<Rgba<u8>, (u32, u32, u32)> = HashMap::new();
+    // 6 for the cube and 2 for the back and front flaps
     let cube_requirement: u32 = 8;
 
     for (x, y, pixel_color) in img.pixels() {
@@ -116,29 +115,28 @@ fn down_scale_image(img: &DynamicImage, new_resolution: &str) -> DynamicImage {
     return resized_image;
 }
 
-fn modify_colors(map: &mut HashMap<Rgba<u8>, (u32, u32, u32)>) {}
+fn reduce_colors(map: &HashMap<Rgba<u8>, (u32, u32, u32)>, amount: u32) {}
 
 fn compare_colors(color1: Rgba<u8>, color2: Rgba<u8>) -> u32 {
-    let [red1, green1, blue1, alpha1] = color1.channels() else {
+    let [red1, green1, blue1] = color1.channels() else {
         panic!("rgba did not have 4 u8 values")
     };
 
-    let [red2, green2, blue2, alpha2] = color2.channels() else {
+    let [red2, green2, blue2] = color2.channels() else {
         panic!("rgba did not have 4 u8 values")
     };
 
-    // can range from 0 to 260100
+    // can range from 0 to 195075
     let difference = (red1 - red2) as u32 * (red1 - red2) as u32
         + (green1 - green2) as u32 * (green1 - green2) as u32
-        + (blue1 - blue2) as u32 * (blue1 - blue2) as u32
-        + (alpha1 - alpha2) as u32 * (alpha1 - alpha2) as u32;
+        + (blue1 - blue2) as u32 * (blue1 - blue2) as u32;
 
     return difference;
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let mut accuracy = -1;
+    let mut max_colors = 0;
 
     if args.len() < 2 {
         panic!("too few command line arguments, a image file path is required")
@@ -152,26 +150,20 @@ fn main() {
 
     for i in (2..args.len()).step_by(2) {
         match args[i].as_str() {
-            "-s" => img = down_scale_image(&img, &args[i + 1]),
-            "--size" => img = down_scale_image(&img, &args[i + 1]),
-            "-a" => {
-                accuracy = args[i + 1]
+            "-r" | "--resolution" => img = down_scale_image(&img, &args[i + 1]),
+            "-m" | "--max" => {
+                max_colors = args[i + 1]
                     .parse()
-                    .expect("invalid input after -a, has to be a positive int")
+                    .expect("invalid input after -m or --max, has to be a positive int");
             }
-            "--accuracy" => {
-                accuracy = args[i + 1]
-                    .parse()
-                    .expect("invalid input after -a, has to be a positive int")
-            }
-            rest => panic!("Invalid command line argument, could not find '{}'", rest),
+            rest => panic!("Invalid command line argument, could not find '{rest}'"),
         }
     }
 
-    let mut colors = get_pixel_info(&img);
+    let colors = get_pixel_info(&img);
 
-    if accuracy != -1 {
-        modify_colors(&mut colors);
+    if max_colors != 0 {
+        reduce_colors(&colors, max_colors);
     }
 
     let mut writer = csv::Writer::from_path("color_data.csv").expect("failed to create csv file");
